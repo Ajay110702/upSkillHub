eba261fac9d78dacbc5c27f2d87479785c6e42d2
Ajay <ajayr5096@gmail.com>
2025-04-05 11:38:11 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors({
   origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["ContenSt-Type", "Authorization"],
 }))
 
 app.use(
